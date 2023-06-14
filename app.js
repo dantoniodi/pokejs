1affdee4c386edde6e0dbaf6f588cea2414966b9
Diego <diego_dnas@hotmail.com>
2023-06-14 14:21:06 -0300

    Calc Damage
    Novo cálculo de dano feito com base na fórmula oficial do jogo. Mais alguns ajustes realizados.

--- app.js
+++ app.js
@@ -1,59 +1,21 @@
-const level = Math.floor(Math.random() * (100 - 5)) + 5
+const level = Math.floor(Math.random() * (99 - 5)) + 5 //both level (5-99)
+const ev = 0 //effort values, since both are wild pokemon their ev will be zero
+const comment = document.getElementById("comment")
 
 class Pokemon {
-  constructor(name, sprite, hp, type, moves) {
+  constructor(name, sprite, hp, stats, type, moves, height, weight) {
     this.name = name;
     this.sprite = sprite;
     this.lv = level;
     this.hp = hp;
     this.fullhp = hp;
+    this.stats = stats;
     this.type = type;
-    this.moves = moves;
+    this.moveset = moves;
+    this.height = height;
+    this.weight = weight;
   }
 }
-
-let pkmList = [
-  [
-    "Charizard",
-    "https://img.pokemondb.net/sprites/black-white/normal/charizard.png",
-    360,
-    [
-      ["Flamethrower", "fire", 95, 0.95],
-      ["Dragon Claw", "dragon", 100, 0.95],
-      ["Air slash", "fly", 75, 0.85],
-      ["Slash", "normal", 70],
-    ],
-  ],
-  [
-    "Blastoise",
-    "https://img.pokemondb.net/sprites/black-white/normal/blastoise.png",
-    362,
-    [
-      ["Surf", "water", 90, 0.95],
-      ["Crunch", "normal", 80, 0.95],
-      ["Ice punch", "ice", 75, 0.95],
-      ["Flash cannon", "steel", 80, 0.95],
-    ],
-  ],
-  [
-    "Venusaur",
-    "https://img.pokemondb.net/sprites/black-white/normal/venusaur-f.png",
-    364,
-    [
-      ["Petal Blizzard", "grass", 90, 0.95],
-      ["Sludge bomb", "poison", 90, 0.95],
-      ["Earthquake", "ground", 100, 0.95],
-      ["Body Slam", "normal", 85, 0.95],
-    ],
-  ],
-];
-
-let typeMatch = {
-  Charizard: [["ground"], ["water", "rock"], ["fire", "grass", "steel"]],
-  Blastoise: [[""], ["grass"], ["fire", "water"]],
-  Venusaur: [["poison"], ["fire", "fly", "ice", "steel"], ["grass", "water"]],
-};
-//console.log(typeMatch);
 
 const getPokemon = id =>
   fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((resp) => resp.json())
@@ -62,67 +24,99 @@ const getMove = url => fetch(url).then((resp) => resp.json())
 
 const setMovePool = async(pool) => {
   let movePool = new Array(4)
+  let moveNum = new Set()
+  while(moveNum.size < 4) {
+    //add random numbers to an unique collection(set)
+    moveNum.add(Math.floor(Math.random() * pool.length) + 1)
+  }
   for(let i=0; i<4; i++) {
-    let moveNum = Math.floor(Math.random() * pool.length)
-    let moveInfo = await getMove(pool[moveNum].move.url)
+    let moveInfo = await getMove(pool[Array.from(moveNum)[i]].move.url)
     movePool.fill([
         moveInfo.name,
         moveInfo.type.name,
+        moveInfo.damage_class.name,
         moveInfo.power != null ? moveInfo.power : 0,
         moveInfo.accuracy != null ? moveInfo.accuracy : 100,
       ], i)
   }
+  //console.log(movePool);
   return movePool
 }
 
 const getType = async(types) => {
+  let test = new Array(1,1)
   let typeMatch = new Array()
   for(let i=0; i<types.length; i++) {
     let data = await fetch(types[i].type.url).then(resp => resp.json())
-    typeMatch.push([
-      data.name,[
+    typeMatch.push([data.name,[
         data.damage_relations.no_damage_from.map(names => names.name),
         data.damage_relations.half_damage_from.map(names => names.name),
         data.damage_relations.double_damage_from.map(names => names.name)
       ]
     ])
   }
-  /*for(i=0;i<typeMatch.length;i++){
-    console.log(typeMatch[i][1])
-    for(x=0;x<typeMatch[i][1].length;x++){
-      if(typeMatch[i][1][x].includes('ground')){
-        switch(x){
-          case 0: console.log('Imune'); break
-          case 1: console.log('Resistente'); break
-          case 2: console.log('Fraco'); break
-        }
-      }
-    }
-  }*/
-  //console.log(typeMatch.length);
+  //console.log(typeMatch);
   return typeMatch
 }
 
-function calcHP(stats) {
+function calcHP(stat) {
   let iv = Math.floor(Math.random() * 31)
-  let ev = Math.floor(Math.random() * 252)
   //Formula found on https://pokemon.fandom.com/wiki/Statistics
-  return Math.floor(0.01 * (2 * stats.base_stat + iv + Math.floor(0.25 * ev)) * level) + level + 10
+  return Math.floor(0.01 * (2 * stat.base_stat + iv + Math.floor(0.25 * ev)) * level) + level + 10
 }
 
-async function spawn(bool) {
-  let p = await getPokemon(Math.floor(Math.random() * 640) + 1);//164
+function calcStat(stats) {
+  let iv = Math.floor(Math.random() * 31)
+  let pokeStats = new Array()
+  for(let i=1; i<stats.length; i++){
+    //Formula found on https://pokemon.fandom.com/wiki/Statistics
+    pokeStats[stats[i].stat.name] = ((Math.floor(0.01 * (2 * stats[i].base_stat + iv + Math.floor(0.25 * ev)) * level) + 5)) //* Nature
+  }
+  return pokeStats
+}
+
+function calcDmg(mov,ata,rec,bon) {
+  let A = level //attacker's Level
+  let B = 0 //attacker's Attack or Special
+  let C = mov[3] //attack Power
+  let D = 0 //defender's Defense or Special
+  let X = 1 //same-type attack bonus (1 or 1.5)
+  let Y = bon //type modifiers (40, 20, 10, 5, 2.5, or 0)
+  let Z = Math.floor(Math.random() * (255 - 217)) + 217 //a random number between 217 and 255
+  switch(mov[2]) {
+    case 'physical': B = ata.stats['attack']; D = rec.stats['defense']; break
+    case 'special': B = ata.stats['special-attack']; D = rec.stats['special-defense']; break
+  }
+  let stab = ata.type.map(typeInfo => typeInfo[0])
+  if (stab.includes(mov[1])) {
+    X = 1.5
+  }
+  let dmg = 0;
+  if(mov[2] !== 'status') {
+    //Formula found on https://www.math.miami.edu/~jam/azure/compendium/battdam.htm
+    dmg = Math.floor(((((((((2*A/5+2)*B*C)/D)/50)+2)*X)*Y/10)*Z)/255)
+  }
+  console.log(dmg);
+  return dmg
+}
+
+async function spawn(bool, id = Math.floor(Math.random() * 640) + 1) {
+  let p = await getPokemon(id)
   let pkm = new Pokemon(
                   p.name,
                   p.sprites,
                   calcHP(p.stats[0]),
-                  await getType(p.types), //p.types.map(typeInfo => typeInfo.type.name),
-                  await setMovePool(p.moves)
+                  calcStat(p.stats),
+                  await getType(p.types),
+                  await setMovePool(p.moves),
+                  p.height,
+                  p.weight
                 )
   //console.log(pkm);
   if (bool) {
     for (i = 0; i < 4; i++) {
-      let moveText = pkm.moves[i][0]+' ('+pkm.moves[i][1]+')'+' [Pwr:'+pkm.moves[i][2]+' | Acc: '+pkm.moves[i][3]+']'
+      let moveText = pkm.moveset[i][0]+' ('+pkm.moveset[i][1]+')'
+        +' [Pwr:'+pkm.moveset[i][3]+' | Acc: '+pkm.moveset[i][4]+']'
       document.getElementById("m" + i).value = moveText;
     }
   }
@@ -134,23 +128,19 @@ async function createPokes() {
   s1 = document.createElement("img");
   s1.src = pk1.sprite.versions['generation-v']['black-white'].animated['back_default'];
   document.getElementById("pk1").appendChild(s1);
-  document.getElementById("hp1").innerHTML =
-    "<p>" + pk1.name
-    +"<br>HP: " + pk1.hp + "/" + pk1.fullhp 
-    +"<br>LV: " + pk1.lv + "</br>";
+  document.getElementById("if1").innerHTML = "<p>" + pk1.name + " LV:" + pk1.lv + "</p>"
+  document.getElementById("hp1").innerHTML = "<p>HP: " + pk1.hp + "/" + pk1.fullhp + "</p>"
 
   let pk2 = await spawn(false);
   s2 = document.createElement("img");
   s2.src = pk2.sprite.versions['generation-v']['black-white'].animated['front_default'];
   document.getElementById("pk2").appendChild(s2);
-  document.getElementById("hp2").innerHTML =
-    "<p>" + pk2.name
-    +"<br>HP: " + pk2.hp + "/" + pk2.fullhp
-    +"<br>LV: " + pk2.lv + "</br>";
+  document.getElementById("if2").innerHTML = "<p>" + pk2.name + " LV:" + pk2.lv + "</p>"
+  document.getElementById("hp2").innerHTML = "<p>HP: " + pk2.hp + "/" + pk2.fullhp + "</p>"
 
   for (i = 0; i < 4; i++) {
     let btn = document.getElementById("m" + i);
-    let move = pk1.moves[i];
+    let move = pk1.moveset[i];
   
     function addHandler(btn, move, pk1, pk2) {
       btn.addEventListener("click", function (e) {
@@ -158,7 +148,7 @@ async function createPokes() {
         setTimeout(
           attack,
           2000,
-          pk2.moves[Math.floor(Math.random() * 3)],
+          pk2.moveset[Math.floor(Math.random() * 3)],
           pk2,
           pk1,
           "hp1",
@@ -171,53 +161,44 @@ async function createPokes() {
 }
 
 function attack(move, attacker, receiver, hp, owner) {
-  document.getElementById("comment").innerHTML +=
-    "<p>" + owner + attacker.name + " used " + move[0] + "!</p>";
-  //console.log((Math.random())+','+(move[3]/100));
-  if (Math.random() <= (move[3]/100)) { //check accuracy
-    let power = (move[2] += Math.floor(Math.random() * 10));
+  comment.innerHTML += "<p>" + owner + attacker.name + " used " + move[0] + "!</p>";
+  if (Math.random() <= (move[4]/100)) { //check accuracy
     let rtype = receiver.type;
     let mtype = move[1];
     let scale = 1;
-
+    
+    //check type effectiveness (attack type x enemy type)
     for (i = 0; i < rtype.length; i++) {
       for (x = 0; x < rtype[i][1].length; x++) {
         if (rtype[i][1][x].includes(mtype)) {
           switch (x) {
-            case 0:
-              scale = 0;
-              setTimeout(function () {
-                document.getElementById("comment").innerHTML +=
-                  "<p>It had no effect!</p>";
-              }, 1000);
-              break;
-            case 1:
-              scale = 0.5;
-              setTimeout(function () {
-                document.getElementById("comment").innerHTML +=
-                  "<p>It was not very effective!</p>";
-              }, 1000);
-              break;
-            case 2:
-              scale = 2;
-              setTimeout(function () {
-                document.getElementById("comment").innerHTML +=
-                  "<p>It was super effective!</p>";
-              }, 1000);
-              break;
+            case 0: scale = scale * 0; break;
+            case 1: scale = scale * 0.5; break;
+            case 2: scale = scale * 2; break;
           }
-          break;
         }
       }
     }
-    let dmgDone = Math.floor(power *= scale)    
-    receiver.hp -= dmgDone
+    scale = scale * 10;
+    if (scale === 0){
+      setTimeout(function () {
+        comment.innerHTML += "<p>It had no effect!</p>";
+      }, 1000);
+    } else if(scale < 10) {
+      setTimeout(function () {
+        comment.innerHTML += "<p>It was not very effective!</p>";
+      }, 1000);
+    } else if(scale > 10){
+      setTimeout(function () {
+        comment.innerHTML += "<p>It was super effective!</p>";
+      }, 1000);
+    }
+    receiver.hp -= calcDmg(move,attacker,receiver,scale)
     document.getElementById(hp).innerHTML =
       "<p>HP: " + receiver.hp + "/" + receiver.fullhp + "</p>";
-    console.log('Dmg done:'+dmgDone);
   } else {
     setTimeout(function () {
-      document.getElementById("comment").innerHTML += "<p>Attack missed!</p>";
+      comment.innerHTML += "<p>Attack missed!</p>";
     });
   }
   checkWinner(attacker,receiver,hp);
@@ -227,11 +208,11 @@ function checkWinner(pk1,pk2,hp) {
   let f = pk1.hp <= 0 ? pk1 : pk2.hp <= 0 ? pk2 : false;
   if (f != false) {
     //alert("GAME OVER: " + f.name + " fainted!");
-    document.getElementById("comment").innerHTML += "<p>GAME OVER: " + f.name + " fainted!</p>";
+    comment.innerHTML += "<p>GAME OVER: " + f.name + " fainted!</p>";
     document.getElementById(hp).innerHTML = "<p>HP: 0/" + f.fullhp + "</p>";
     setTimeout(function () {
       location.reload();
-    }, 5000);
+    }, 1500);
   }
 }
 
